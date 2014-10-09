fd2d233f4a240ecb838c3502461b5ce93a22f6c4
Christoffer Winterkvist <chris@zenangst.com>
2014-10-09 21:37:25 +0200

    Add test skeleton

--- NSManagedObject-HYPPropertyMapperTests/NSManagedObject_HYPPropertyMapperTests.m
+++ NSManagedObject-HYPPropertyMapperTests/NSManagedObject_HYPPropertyMapperTests.m
@@ -6,29 +6,58 @@
 //
 //
 
+#import <CoreData/CoreData.h>
 #import <XCTest/XCTest.h>
 
 @interface NSManagedObject_HYPPropertyMapperTests : XCTestCase
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
 
 @end
 
 @implementation NSManagedObject_HYPPropertyMapperTests
 
+#pragma mark - Set up
+
++ (NSManagedObjectContext *)managedObjectContextForTests
+{
+    static NSManagedObjectModel *model = nil;
+    if (!model) {
+        model = [NSManagedObjectModel mergedModelFromBundles:[NSBundle allBundles]];
+    }
+
+    NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+    NSPersistentStore *store = [psc addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:nil options:nil error:nil];
+    NSAssert(store, @"Should have a store by now");
+
+    NSManagedObjectContext *moc = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    moc.persistentStoreCoordinator = psc;
+
+    return moc;
+}
+
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.managedObjectContext = [NSManagedObject_HYPPropertyMapperTests managedObjectContextForTests];
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [self.managedObjectContext rollback];
     [super tearDown];
 }
 
-- (void)testExample
+#pragma mark - Tests
+
+- (void)testDictionaryRepresentation
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    NSManagedObject *user = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:self.managedObjectContext];
+
+    [user setValue:@"John" forKey:@"firstName"];
+    [user setValue:@"Hyperseed" forKey:@"lastName"];
+
+    
 }
 
 @end
