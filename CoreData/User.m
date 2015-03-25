b0fa33713b84df411c2ab7499a4f57fc271856d7
Elvis Nuñez <elvisnunez@me.com>
2015-03-25 20:31:25 +0100

    Clean up models

--- CoreData/User.m
+++ CoreData/User.m
@@ -2,20 +2,19 @@
 #import "Company.h"
 #import "Note.h"
 
-
 @implementation User
 
 @dynamic age;
 @dynamic birthDate;
 @dynamic contractID;
-@dynamic createdDate;
+@dynamic createdAt;
 @dynamic driverIdentifier;
 @dynamic firstName;
 @dynamic ignoredParameter;
 @dynamic lastName;
 @dynamic numberOfAttendes;
 @dynamic remoteID;
-@dynamic updatedDate;
+@dynamic updatedAt;
 @dynamic userDescription;
 @dynamic userType;
 @dynamic hobbies;
