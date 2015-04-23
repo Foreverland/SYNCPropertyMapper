aa574644011b15858965cb7d58049980c55faa19
Elvis Nuñez <elvisnunez@me.com>
2015-04-23 17:23:49 +0200

    Add private tests

--- Pod/Tests/PrivateTests.m
+++ Pod/Tests/PrivateTests.m
@@ -3,6 +3,12 @@
 
 #import "NSManagedObject+HYPPropertyMapper.h"
 
+#import "User.h"
+#import "Note.h"
+#import "Company.h"
+#import "Market.h"
+#import "DATAStack.h"
+
 @interface NSManagedObject (PrivateMethods)
 
 - (NSAttributeDescription *)attributeDescriptionForKey:(NSString *)key;
@@ -17,5 +23,27 @@
 @end
 
 @implementation PrivateTests
+
+- (id)entityNamed:(NSString *)entityName {
+    return [NSEntityDescription insertNewObjectForEntityForName:entityName
+                                         inManagedObjectContext:self.managedObjectContext];
+}
+
+- (NSManagedObjectContext *)managedObjectContext {
+    DATAStack *dataStack = [[DATAStack alloc] initWithModelName:@"Model"
+                                                         bundle:[NSBundle bundleForClass:[self class]]
+                                                      storeType:DATAStackInMemoryStoreType];
+    return dataStack.mainContext;
+}
+
+- (void)testAttributeDescriptionForKey {
+    Market *market = [self entityNamed:@"Market"];
+    [market attributeDescriptionForKey:@"uniqueId"];
+    [market attributeDescriptionForKey:@"otherAttribute"];
+}
+
+- (void)testValueForAttributeDescription {
+
+}
 
 @end
