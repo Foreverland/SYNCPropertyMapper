eabb1292eba112d4d58d3c6ccf01edbc20b2b7db
Elvis Nuñez <elvisnunez@me.com>
2015-08-23 11:40:13 +0200

    Rollback date parsing changes

--- Source/NSManagedObject+HYPPropertyMapper.m
+++ Source/NSManagedObject+HYPPropertyMapper.m
@@ -270,74 +270,48 @@ static NSString * const HYPPropertyMapperTimestamp = @"T00:00:00+00:00";
 @implementation NSDate (HYPISO8601)
 
 + (NSDate *)hyp_dateFromISO8601String:(NSString *)iso8601 {
-    if (!iso8601 || [iso8601 isEqual:[NSNull null]]) {
-        return nil;
-    }
+    if (!iso8601 || [iso8601 isEqual:[NSNull null]]) return nil;
 
-    // Parse number
     if ([iso8601 isKindOfClass:[NSNumber class]]) {
         return [NSDate dateWithTimeIntervalSince1970:[(NSNumber *)iso8601 doubleValue]];
-    }
+    } else if ([iso8601 isKindOfClass:[NSString class]]) {
 
-    // Parse string
-    else if ([iso8601 isKindOfClass:[NSString class]]) {
+        if (!iso8601) return nil;
+
         const char *str = [iso8601 cStringUsingEncoding:NSUTF8StringEncoding];
-        size_t len = strlen(str);
-        if (len == 0) {
-            return nil;
-        }
+        char newStr[25];
 
         struct tm tm;
-        char newStr[25] = "";
-        BOOL hasTimezone = NO;
+        size_t len = strlen(str);
 
-        // 2014-03-30T09:13:00Z
-        if (len == 20 && str[len - 1] == 'Z') {
+        if (len == 0) return nil;
+
+        BOOL UTC = (len == 20 && str[len - 1] == 'Z');
+        BOOL miliseconds = (len == 24 && str[len - 1] == 'Z');
+        BOOL timezone = (len == 25 && str[22] == ':');
+        if (UTC) {
             strncpy(newStr, str, len - 1);
-        }
-
-        // 2014-03-30T09:13:00-07:00
-        else if (len == 25 && str[22] == ':') {
-            strncpy(newStr, str, 19);
-            hasTimezone = YES;
-        }
-
-        // 2014-03-30T09:13:00.000Z
-        else if (len == 24 && str[len - 1] == 'Z') {
-            strncpy(newStr, str, 19);
-        }
-
-        // 2014-03-30T09:13:00.000-07:00
-        else if (len == 29 && str[26] == ':') {
-            strncpy(newStr, str, 19);
-            hasTimezone = YES;
-        }
-
-        // Poorly formatted timezone
-        else {
+            strncpy(newStr + len - 1, "+0000", 5);
+        } else if (miliseconds) {
+            strncpy(newStr, str, len - 1);
+            strncpy(newStr, str, len - 5);
+            strncpy(newStr + len - 5, "+0000", 5);
+        } else if (timezone) {
+            strncpy(newStr, str, 22);
+            strncpy(newStr + 22, str + 23, 2);
+        } else {
             strncpy(newStr, str, len > 24 ? 24 : len);
         }
-
-        // Timezone
-        size_t l = strlen(newStr);
-        if (hasTimezone) {
-            strncpy(newStr + l, str + len - 6, 3);
-            strncpy(newStr + l + 3, str + len - 2, 2);
-        } else {
-            strncpy(newStr + l, "+0000", 5);
-        }
         
-        // Add null terminator
         newStr[sizeof(newStr) - 1] = 0;
         
-        if (strptime(newStr, "%FT%T%z", &tm) == NULL) {
-            return nil;
-        }
+        if (strptime(newStr, "%FT%T%z", &tm) == NULL) return nil;
         
         time_t t;
         t = mktime(&tm);
         
-        return [NSDate dateWithTimeIntervalSince1970:t];
+        NSDate *returnedDate = [NSDate dateWithTimeIntervalSince1970:t];
+        return returnedDate;
     }
     
     NSAssert1(NO, @"Failed to parse date: %@", iso8601);
