25519ec4bd07c6e8bcfb51f93c2e2eadcbf45567
Elvis Nuñez <elvisnunez@me.com>
2015-09-24 15:54:40 +0200

    Improve documentation

--- Source/NSManagedObject+HYPPropertyMapper.h
+++ Source/NSManagedObject+HYPPropertyMapper.h
@@ -4,6 +4,10 @@ static NSString * const HYPPropertyMapperCustomRemoteKey = @"hyper.remoteKey";
 
 @interface NSManagedObject (HYPPropertyMapper)
 
+/*! Fills the NSManagedObject with the contents of the dictionary
+ *  using a convention-over-configuration paradigm mapping the
+ *  Core Data attributes to their conterparts in JSON using snake_case.
+ */
 - (void)hyp_fillWithDictionary:(NSDictionary *)dictionary;
 
 /*! Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Includes relationships to other models using Ruby on Rail's nested attributes model.
@@ -19,13 +23,13 @@ static NSString * const HYPPropertyMapperCustomRemoteKey = @"hyper.remoteKey";
  */
 - (NSDictionary *)hyp_dictionaryIncludingNestedAttributes:(BOOL)shouldIncludeNestedAttributes;
 
-/*! Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Includes relationships to other models.
+/*! Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Includes relationships to other models using Ruby on Rail's nested attributes model.
  * \param dateFormatter - A custom date formatter that turns NSDate objects into NSString objects. Do not pass nil, instead use the 'hyp_dictionary' method
  * \return NSDictionary of values that can be serialized
  */
 - (NSDictionary *)hyp_dictionaryWithDateFormatter:(NSDateFormatter *)formatter;
 
-/*! Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Includes relationships to other models.
+/*! Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Could include relationships to other models using Ruby on Rail's nested attributes model.
  * \param dateFormatter - A custom date formatter that turns NSDate objects into NSString objects. Do not pass nil, instead use the 'hyp_dictionary' method
  * \param shouldIncludeNestedAttributes - A boolean to indicate weather the NSDictionary should include nested attributes or not.
  * \return NSDictionary of values that can be serialized
