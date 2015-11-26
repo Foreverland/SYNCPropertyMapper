734018b339194767a7026e2f0427f5619a5891d8
Elvis Nuñez <elvisnunez@me.com>
2015-11-26 15:37:29 +0100

    Improve date with milliseconds handling
    Fixes #88

--- Tests/DateTests.swift
+++ Tests/DateTests.swift
@@ -4,20 +4,15 @@ import NSManagedObject_HYPPropertyMapper
 
 class DateTests: XCTestCase {
     func testDateA() {
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-        formatter.timeZone = NSTimeZone(forSecondsFromGMT: 2 * 60 * 60)
-
-        let date = formatter.dateFromString("2015-06-23T12:40:08.000")
+        let date = NSDate.dateWithHourAndTimeZoneString("2015-06-23T12:40:08.000")
         let resultDate = NSDate.hyp_dateFromDateString("2015-06-23T12:40:08.000+02:00")
 
         XCTAssertNotNil(resultDate)
-        XCTAssertEqualWithAccuracy(resultDate.timeIntervalSinceReferenceDate, date!.timeIntervalSinceReferenceDate, accuracy: 1.0)
+        XCTAssertEqualWithAccuracy(resultDate.timeIntervalSinceReferenceDate, date.timeIntervalSinceReferenceDate, accuracy: 1.0)
     }
 
     func testDateB() {
-        let formatter = NSDateFormatter.GMTFormatter()
-        let date = formatter.dateFromString("2014-01-01")
+        let date = NSDate.dateWithDayString("2014-01-01")
         let resultDate = NSDate.hyp_dateFromDateString("2014-01-01T00:00:00+00:00")
 
         XCTAssertNotNil(resultDate)
@@ -25,8 +20,7 @@ class DateTests: XCTestCase {
     }
 
     func testDateC() {
-        let formatter = NSDateFormatter.GMTFormatter()
-        let date = formatter.dateFromString("2014-01-02")
+        let date = NSDate.dateWithDayString("2014-01-02")
         let resultDate = NSDate.hyp_dateFromDateString("2014-01-02")
 
         XCTAssertNotNil(resultDate)
@@ -34,8 +28,7 @@ class DateTests: XCTestCase {
     }
 
     func testDateD() {
-        let formatter = NSDateFormatter.GMTFormatter()
-        let date = formatter.dateFromString("2014-01-02")
+        let date = NSDate.dateWithDayString("2014-01-02")
         let resultDate = NSDate.hyp_dateFromDateString("2014-01-02T00:00:00.007450+00:00")
 
         XCTAssertNotNil(resultDate)
@@ -43,17 +36,23 @@ class DateTests: XCTestCase {
     }
 
     func testDateE() {
-        let formatter = NSDateFormatter.GMTFormatter()
-        let date = formatter.dateFromString("2015-09-10")
+        let date = NSDate.dateWithDayString("2015-09-10")
         let resultDate = NSDate.hyp_dateFromDateString("2015-09-10T00:00:00.116+0000")
 
         XCTAssertNotNil(resultDate)
         XCTAssertEqual(date, resultDate)
     }
 
+    func testDateF() {
+        let date = NSDate.dateWithDayString("2015-09-10")
+        let resultDate = NSDate.hyp_dateFromDateString("2015-09-10T00:00:00.184968Z")
+
+        XCTAssertNotNil(resultDate)
+        XCTAssertEqual(date, resultDate)
+    }
+
     func testTimestampA() {
-        let formatter = NSDateFormatter.GMTFormatter()
-        let date = formatter.dateFromString("2015-09-10")
+        let date = NSDate.dateWithDayString("2015-09-10")
         let resultDate = NSDate.hyp_dateFromDateString("1441843200")
 
         XCTAssertNotNil(resultDate)
@@ -61,8 +60,7 @@ class DateTests: XCTestCase {
     }
 
     func testTimestampB() {
-        let formatter = NSDateFormatter.GMTFormatter()
-        let date = formatter.dateFromString("2015-09-10")
+        let date = NSDate.dateWithDayString("2015-09-10")
         let resultDate = NSDate.hyp_dateFromDateString("1441843200000000")
 
         XCTAssertNotNil(resultDate)
@@ -70,8 +68,7 @@ class DateTests: XCTestCase {
     }
 
     func testTimestampC() {
-        let formatter = NSDateFormatter.GMTFormatter()
-        let date = formatter.dateFromString("2015-09-10")
+        let date = NSDate.dateWithDayString("2015-09-10")
         let resultDate = NSDate.hyp_dateFromUnixTimestampNumber(1441843200)
 
         XCTAssertNotNil(resultDate)
@@ -79,8 +76,7 @@ class DateTests: XCTestCase {
     }
 
     func testTimestampD() {
-        let formatter = NSDateFormatter.GMTFormatter()
-        let date = formatter.dateFromString("2015-09-10")
+        let date = NSDate.dateWithDayString("2015-09-10")
         let resultDate = NSDate.hyp_dateFromUnixTimestampNumber(1441843200000000)
 
         XCTAssertNotNil(resultDate)
@@ -96,12 +92,22 @@ class DateTests: XCTestCase {
     }
 }
 
-extension NSDateFormatter {
-    static func GMTFormatter() -> NSDateFormatter {
+extension NSDate {
+    static func dateWithDayString(dateString: String) -> NSDate {
         let formatter = NSDateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         formatter.timeZone = NSTimeZone(name: "GMT")
+        let date = formatter.dateFromString(dateString)!
 
-        return formatter
+        return date
+    }
+
+    static func dateWithHourAndTimeZoneString(dateString: String) -> NSDate {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        formatter.timeZone = NSTimeZone(forSecondsFromGMT: 2 * 60 * 60)
+        let date = formatter.dateFromString(dateString)!
+
+        return date
     }
 }
