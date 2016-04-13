43e6da71dd100f732afd8b3c6f3416879a9699fc
Elvis Nuñez <elvisnunez@me.com>
2016-04-13 13:03:33 +0200

    Clean up transformer

--- Tests/Models/DateStringTransformer.h
+++ Tests/Models/DateStringTransformer.h
@@ -1,15 +1,6 @@
-//
-//  DateStringTransformer.h
-//  Demo
-//
-//  Created by Aleksandr Kelbas on 13/04/2016.
-//
-//
+@import Foundation;
 
-#import <Foundation/Foundation.h>
-
-
-/*
+/**
  This class is transforming "/Date(1460537233000)/" string into an NSDate object that can be stored in Core Data
  */
 @interface DateStringTransformer : NSValueTransformer
