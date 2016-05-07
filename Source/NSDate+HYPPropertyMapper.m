2dd61a83ebd311e7804e1cdb87af6a89bd4cdbc2
Jeremy Bone <jeremy@bonebox.org>
2016-05-06 19:26:01 -0500

    Update NSDate+HYPPropertyMapper.m
    Use NSDateFormatter to achieve millisecond precision for ISO8601 string conversions.

--- Source/NSDate+HYPPropertyMapper.m
+++ Source/NSDate+HYPPropertyMapper.m
@@ -32,80 +32,54 @@
             dateString = [mutableRemoteValue copy];
         }
 
+        NSDateFormatter *dateFormatter = [NSDateFormatter new];
+        dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+
         const char *str = [dateString cStringUsingEncoding:NSUTF8StringEncoding];
         size_t len = strlen(str);
         if (len == 0) {
             return nil;
         }
 
-        struct tm tm;
-        char newStr[25] = "";
-        BOOL hasTimezone = NO;
-
         // 2014-03-30T09:13:00Z
         if (len == 20 && str[len - 1] == 'Z') {
-            strncpy(newStr, str, len - 1);
+            dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
         }
 
         // 2014-03-30T09:13:00-07:00
         else if (len == 25 && str[22] == ':') {
-            strncpy(newStr, str, 19);
-            hasTimezone = YES;
+            dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZZZZZ";
         }
 
         // 2014-03-30T09:13:00.000Z
         else if (len == 24 && str[len - 1] == 'Z') {
-            strncpy(newStr, str, 19);
+            dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSSZ";
         }
 
         // 2015-06-23T12:40:08.000+02:00
         else if (len == 29 && str[26] == ':') {
-            strncpy(newStr, str, 19);
-            hasTimezone = YES;
+            dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ";
         }
 
         // 2015-08-23T09:29:30.007450+00:00
         else if (len == 32 && str[29] == ':') {
-            strncpy(newStr, str, 19);
-            hasTimezone = YES;
+            dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSSSSSZZZZZ";
         }
 
         // 2015-09-10T13:47:21.116+0000
         else if (len == 28 && str[23] == '+') {
-            strncpy(newStr, str, 19);
-            hasTimezone = NO;
-        }
-
-        // 2015-09-10T00:00:00.XXXXXXZ
-        else if (str[19] == '.' && str[len - 1] == 'Z') {
-            strncpy(newStr, str, 19);
+            dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSSZZZ";
         }
 
         // Poorly formatted timezone
         else {
-            strncpy(newStr, str, len > 24 ? 24 : len);
+            if (dateString.length > 19) {
+                dateString = [[dateString substringToIndex:18] stringByAppendingString:@"Z"];
+            }
+            dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
         }
 
-        // Timezone
-        size_t l = strlen(newStr);
-        if (hasTimezone) {
-            strncpy(newStr + l, str + len - 6, 3);
-            strncpy(newStr + l + 3, str + len - 2, 2);
-        } else {
-            strncpy(newStr + l, "+0000", 5);
-        }
-
-        // Add null terminator
-        newStr[sizeof(newStr) - 1] = 0;
-
-        if (strptime(newStr, "%FT%T%z", &tm) == NULL) {
-            return nil;
-        }
-
-        time_t t;
-        t = mktime(&tm);
-
-        return [NSDate dateWithTimeIntervalSince1970:t];
+        return [dateFormatter dateFromString:dateString];
     }
 
     NSAssert1(NO, @"Failed to parse date: %@", dateString);
