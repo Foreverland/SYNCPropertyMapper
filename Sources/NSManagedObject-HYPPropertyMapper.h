a14c73750ea36abdaaf542017f7e8193ace9c235
Elvis Nuñez <elvisnunez@me.com>
2016-10-04 19:23:51 +0200

    Remove unused

--- Sources/NSManagedObject-HYPPropertyMapper.h
+++
@@ -1,4 +0,0 @@
-@import Foundation;
-
-FOUNDATION_EXPORT double NSManagedObject-HYPPropertyMapperVersionNumber;
-FOUNDATION_EXPORT const unsigned char NSManagedObject-HYPPropertyMapperVersionString[];
