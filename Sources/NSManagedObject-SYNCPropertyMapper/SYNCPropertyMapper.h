2c85176ca5efd36683ac1ca263ec20a483a25f9c
Elvis Nuñez <elvisnunez@me.com>
2016-10-23 02:02:09 +0200

    Even better headers

--- Sources/NSManagedObject-SYNCPropertyMapper/SYNCPropertyMapper.h
+++ Sources/NSManagedObject-SYNCPropertyMapper/SYNCPropertyMapper.h
@@ -24,64 +24,62 @@ typedef NS_ENUM(NSInteger, SYNCPropertyMapperRelationshipType) {
 };
 
 /**
- Collection of helper methods to facilitate dealing with JSON and NSManagedObjects.
+ Collection of helper methods to facilitate mapping JSON to NSManagedObject.
  */
 @interface NSManagedObject (SYNCPropertyMapper)
 
 /**
- Fills the NSManagedObject with the contents of the dictionary
- using a convention-over-configuration paradigm mapping the
- Core Data attributes to their conterparts in JSON using snake_case.
+ Fills the @c NSManagedObject with the contents of the dictionary using a convention-over-configuration paradigm mapping the Core Data attributes to their conterparts in JSON using snake_case.
 
- @param dictionary The JSON dictionary to be used to fill the values of your NSManagedObject.
+ @param dictionary The JSON dictionary to be used to fill the values of your @c NSManagedObject.
  */
 - (void)hyp_fillWithDictionary:(NSDictionary<NSString *, id> *)dictionary;
 
 /**
- Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Includes relationships to other models using Ruby on Rail's nested attributes model.
- NSDate objects will be stringified to the ISO-8601 standard.
+ Creates a @c NSDictionary of values based on the @c NSManagedObject subclass that can be serialized by @c NSJSONSerialization. Includes relationships to other models using Ruby on Rail's nested attributes model.
+ @c NSDate objects will be stringified to the ISO-8601 standard.
 
- @return The JSON representation of the NSManagedObject in the form of a NSDictionary.
+ @return The JSON representation of the @c NSManagedObject in the form of a @c NSDictionary.
  */
 - (NSDictionary<NSString *, id> *)hyp_dictionary;
 
 /**
- Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Could include relationships to other models.
- NSDate objects will be stringified to the ISO-8601 standard.
+ Creates a @c NSDictionary of values based on the @c NSManagedObject subclass that can be serialized by @c NSJSONSerialization. Could include relationships to other models.
+ @c NSDate objects will be stringified to the ISO-8601 standard.
 
- @param relationshipType It indicates wheter the result dictionary should include no relationships, nested attributes or normal attributes
+ @param relationshipType It indicates wheter the result dictionary should include no relationships, nested attributes or normal attributes.
 
- @return The JSON representation of the NSManagedObject in the form of a NSDictionary.
+ @return The JSON representation of the @c NSManagedObject in the form of a @c NSDictionary.
  */
 - (NSDictionary<NSString *, id> *)hyp_dictionaryUsingRelationshipType:(SYNCPropertyMapperRelationshipType)relationshipType;
 
 /**
- Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Includes relationships to other models using Ruby on Rail's nested attributes model.
+ Creates a @c NSDictionary of values based on the @c NSManagedObject subclass that can be serialized by @c NSJSONSerialization. Includes relationships to other models using Ruby on Rail's nested attributes model.
 
- @param dateFormatter A custom date formatter that turns NSDate objects into NSString objects. Do not pass nil, instead use the 'hyp_dictionary' method
+ @param dateFormatter A custom date formatter that turns @c NSDate objects into NSString objects. Do not pass @c nil, instead use the @c hyp_dictionary method.
 
- @return The JSON representation of the NSManagedObject in the form of a NSDictionary.
+ @return The JSON representation of the @c NSManagedObject in the form of a @c NSDictionary.
  */
 - (NSDictionary<NSString *, id> *)hyp_dictionaryWithDateFormatter:(NSDateFormatter *)dateFormatter;
 
 /**
- Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Could include relationships to other models using Ruby on Rail's nested attributes model.
+ Creates a @c NSDictionary of values based on the @c NSManagedObject subclass that can be serialized by @c NSJSONSerialization. Could include relationships to other models using Ruby on Rail's nested attributes model.
 
- @param dateFormatter    A custom date formatter that turns NSDate objects into NSString objects. Do not pass nil, instead use the 'hyp_dictionary' method
- @param relationshipType It indicates wheter the result dictionary should include no relationships, nested attributes or normal attributes
+ @param dateFormatter    A custom date formatter that turns @c NSDate objects into @c NSString objects. Do not pass nil, instead use the 'hyp_dictionary' method.
+ @param relationshipType It indicates wheter the result dictionary should include no relationships, nested attributes or normal attributes.
 
- @return The JSON representation of the NSManagedObject in the form of a NSDictionary.
+ @return The JSON representation of the @c NSManagedObject in the form of a @c NSDictionary.
  */
 - (NSDictionary<NSString *, id> *)hyp_dictionaryWithDateFormatter:(NSDateFormatter *)dateFormatter usingRelationshipType:(SYNCPropertyMapperRelationshipType)relationshipType;
 
 /**
- Creates a NSDictionary of values based on the NSManagedObject subclass that can be serialized by NSJSONSerialization. Could include relationships to other models using Ruby on Rail's nested attributes model.
+ Creates a @c NSDictionary of values based on the @c NSManagedObject subclass that can be serialized by @c NSJSONSerialization. Could include relationships to other models using Ruby on Rail's nested attributes model.
 
- @param dateFormatter    A custom date formatter that turns NSDate objects into NSString objects. Do not pass nil, instead use the 'hyp_dictionary' method
- @param parent           The parent of the managed object
- @param relationshipType It indicates wheter the result dictionary should include no relationships, nested attributes or normal attributes
+ @param dateFormatter    A custom date formatter that turns @c NSDate objects into @c NSString objects. Do not pass nil, instead use the @c hyp_dictionary method.
+ @param parent           The parent of the managed object.
+ @param relationshipType It indicates wheter the result dictionary should include no relationships, nested attributes or normal attributes.
 
- @return The JSON representation of the NSManagedObject in the form of a NSDictionary.
+ @return The JSON representation of the @c NSManagedObject in the form of a @c NSDictionary.
  */
 - (NSDictionary<NSString *, id> *)hyp_dictionaryWithDateFormatter:(NSDateFormatter *)dateFormatter parent:( NSManagedObject * _Nullable )parent usingRelationshipType:(SYNCPropertyMapperRelationshipType)relationshipType;
 
